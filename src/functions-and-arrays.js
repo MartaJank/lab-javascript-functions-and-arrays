3459cd5ad0e8882b3a8db1928cc9f53d115686db
Marta Jankowska <martajan218@gmail.com>
2020-10-05 19:24:17 +0200

    all exercices done + bonuses (except the last one)

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -2,9 +2,11 @@
 
 function maxOfTwoNumbers(num1, num2){
   if (num1 > num2){
-    console.log('El número más grande es ' + num1)
-  }else {
-    console.log('El número más grande es ' + num2)
+    return num1;
+  } else if (num1 < num2){
+    return num2;
+  } else {
+    return num1;
   }
 }
 
@@ -14,41 +16,60 @@ maxOfTwoNumbers(6, 9);
 
 function findLongestWord(array){
   let word = "";
+  if(array[0]){
   for (let i = 0; i < array.length; i++) {
     if (word.length < array[i].length) {
       word = array[i];
     }
   }
-  return word;
+    return word;
+  } else {
+    return null;
+}
 }
 
 const words = ['mystery', 'brother', 'pristrdew', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot']
 
 findLongestWord(words);
 
-//Interation 3
+//Iteration 3
 
 function sumNumbers(array){
-  var total =  0;
+  var total = 0;
 
-            for(var i=0;i<array.length;i++)
-              {                  
-                if(isNaN(array[i])){
-                continue;
-                 }
-                  total += Number(array[i]);
-               }
-             return total;
-}
+  for(var i = 0; i < array.length; i++){                  
+    if(typeof array[i] !== 'number'){
+      continue;
+    }
+      total += array[i];
+    }
+      return total;
+}            
 
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10]
 sumNumbers(numbers);
 
-//Interaction 4
+//Bonus Iteration 3.1
+function sum(array) {
+  var total = 0;
+
+  for(var i = 0; i < array.length; i++) {                  
+      if (typeof array[i] === 'object') {
+        throw new Error("Unsupported data type sir or ma'am");
+      } else if (typeof array[i] === 'string') {
+        total += array[i].length
+      }else {
+        total += array[i];
+      } 
+    }
+  return total;
+}
+
+//Iteration 4
 //Level 1
 function averageNumbers(array) {
     if (array.length === 0 ) {
-        return;
+        return null;
     }
     return sumNumbers(array) / array.length;
 }
@@ -57,24 +78,46 @@ const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 averageNumbers(numbers2)
 
 //Level 2
-
 function averageWordLength(array){
-if (array.length === 0) {
-  return undefined
-}
-var suma = 0;
-for(var x = 0; x < array.length; x++){
-  suma += array[x].length;
-} 
-var promedio = suma / array.length;
-return promedio
+  if (array.length === 0) {
+    return null
+  }
+  var suma = 0;
+  for(var x = 0; x < array.length; x++){
+    suma += array[x].length;
+  } 
+  var promedio = suma / array.length;
+  return promedio
 }
 
 const words2 = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 averageWordLength(words2);
 
-//Iteration 5
+//Bonus Iteration 4.1
+function avg(arr) {
+  if (arr.length === 0) {
+    return null
+  }
+  var sum = 0;
 
+  for(var i = 0; i < arr.length; i++) {                  
+      if (typeof arr[i] === 'object') {
+        throw new Error("Unsupported data type sir or ma'am");
+      } else if (typeof arr[i] === 'string') {
+        sum += arr[i].length
+      } else {
+        sum += arr[i];
+      } 
+    }
+  let average = sum / arr.length;
+  return Number(average.toFixed(2))
+}
+
+const mixedArr = [6, 12, 'miami', 1, true, 'barca', '200', 'lisboa', 8, 10];
+
+avg(mixedArr);
+
+//Iteration 5
 const words3 = [
   'crab',
   'poison',
@@ -90,23 +133,29 @@ const words3 = [
 ];
 
 
-function uniquifyArray(value, index, self) { 
-    return self.indexOf(value) === index;
+function uniquifyArray(array) { 
+  if (array.length === 0) {
+    return null
+  }
+  
+  const unique = new Set(array);
+  const backToArray = [...unique];
+    return backToArray;
 }
 
-var unique = words3.filter( uniquifyArray ); 
-console.info(unique)
-
 //Iteration 6
-
-function doesWordExist (array, term){ 
+function doesWordExist (array, term){
+  if (array.length === 0) {
+    return null
+  }
+  
   for(var i=0; i<array.length; i++) {
     if (array[i] === term) {
       return true
      }
     }
- return false
-  }
+  return false
+}
 
 
 const words4 = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
@@ -114,7 +163,6 @@ const words4 = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 
 doesWordExist (words4, 'parrot');
 
 //Iteration 7
-
 function howManyTimes (array, term) {
   var n = 0;
     for(i = 0; i < array.length; i++){
@@ -139,3 +187,4 @@ const words5 = [
 ];
 
 howManyTimes (words5, 'disobedience');
+
